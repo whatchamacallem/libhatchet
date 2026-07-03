5b07167319f22e262092979f24d0ca8ab118f509
A Johnston <ajohnston54637@gmail.com>
2026-07-02 23:40:53 -0700

    v1.45.5

--- include/hx/libhatchet.h
+++ include/hx/libhatchet.h
@@ -41,10 +41,10 @@
 
 /// `int LIBHATCHET_VER` - One digit major, and two digit minor and patch
 /// versions.
-#define LIBHATCHET_VER 14504
+#define LIBHATCHET_VER 14505
 
 /// `LIBHATCHET_TAG` - Major, minor and patch version tag name.
-#define LIBHATCHET_TAG "v1.45.4"
+#define LIBHATCHET_TAG "v1.45.5"
 
 #if !defined HX_HARDENING_MODE
 /// `HX_HARDENING_MODE` - Library hardening level. See the README.md for levels.
